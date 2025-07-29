85be01316a88759c3f2fc2dec79de01d5188ed07
Your Name <you@example.com>
2025-07-29 21:31:07 +0200

    .

--- components/dooya/cover/dooya_cover.h
+++ components/dooya/cover/dooya_cover.h
@@ -10,21 +10,19 @@
 namespace esphome {
 namespace dooya_cover {
 
-class DooyaCover : public Component, public cover::Cover, public Parented<DooyaBridge>
+class DooyaCover : public Component, public cover::Cover, public Parented<dooya_bridge::DooyaBridge>
 {
  public:
   void setup() override;
   void loop() override;
   void dump_config() override;
   cover::CoverTraits get_traits() override;
-  void set_parent(dooya_bridge::DooyaBridge *parent) { this->parent_ = parent; }
   void set_address(std::string address) { address_ = address; }
   
  protected:
   void control(const cover::CoverCall &call) override;
   void parse_rx(std::string rx);
 
-  dooya_bridge::DooyaBridge *parent_;
   std::string address_;
   optional<float> new_position_;
   optional<float> new_tilt_;
